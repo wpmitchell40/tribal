423c933ba2634be5fca3b2b719a2d306173e3d3a
wpmitchell40 <p@rkermitchell.com>
2018-06-22 15:28:20 -0600

    using realBody

--- main.go
+++ main.go
@@ -133,7 +133,7 @@ func (s TribalServer) RequestPostHandler(w http.ResponseWriter, r *http.Request)
 			fmt.Println(err)
 			panic(err)
 		}
-		realBody := body[11:len(body)-3]
+		realBody := body[14:len(body)-6]
 		fmt.Println(string(realBody))
 		c, err := slackevents.ParseActionEvent(string(realBody))
 		if err != nil {
